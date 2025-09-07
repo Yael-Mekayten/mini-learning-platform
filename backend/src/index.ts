ee9aec75dbcddf616309eeda6a646a177448c49f
Yael-Mekayten <yaelmekayten2005@gmail.com>
2025-09-07 22:38:43 +0300

    Refactor: use RESTful route for creating subcategories (/categories/:id/subcategories)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,32 +1,20 @@
 import express from "express";
-import prisma from "./prisma";
+import userRoutes from "./routes/users";
+import categoryRoutes from "./routes/categories";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Health check
 app.get("/", (req, res) => {
   res.send("Mini Learning Platform API is running 🚀");
 });
-//new user creation
-app.post("/users", async (req, res) => {
-  const { name, phone } = req.body;
-  try {
-    const user = await prisma.user.create({
-      data: { name, phone },
-    });
-    res.json(user);
-  } catch (error) {
-    res.status(400).json({ error: "Could not create user" });
-  }
-});
 
-// get all users
-app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
+// Routes
+app.use("/users", userRoutes);
+app.use("/categories", categoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
