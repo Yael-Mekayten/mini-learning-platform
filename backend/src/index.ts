ddcf375f665afda62a4ce81489e62f2f9c058690
Yael-Mekayten <yaelmekayten2005@gmail.com>
2025-09-09 20:37:15 +0300

    add midllwares for the app cors helmet morgan

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,39 @@
-
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
 import userRoutes from "./routes/users";
 import categoryRoutes from "./routes/categories";
 import promptRoutes from "./routes/prompts";
-import adminRoutes from "./routes/adminRoutes";
+import adminRoutes from "./routes/admin";
+import authRoutes from "./routes/auth";
+
 import { errorHandler } from "./middleware/errorHandler";
 import { notFound } from "./middleware/notFound";
-import authRoutes from "./routes/auth";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
 
 // Routes
-app.use("/prompts", promptRoutes);
-app.use("/users", userRoutes);
-app.use("/categories", categoryRoutes);
-app.use("/admin", adminRoutes);
-app.use("/api/auth", authRoutes);   
-app.use(errorHandler);
-
+app.use("/api/prompts", promptRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
 
 // Health check
 app.get("/", (req, res) => {
   res.send("Mini Learning Platform API is running 🚀");
 });
+
+// Not found + Error handler
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
